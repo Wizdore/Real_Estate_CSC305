1507c446ac420078072ff1cab4628e1860e7f330
nocmnt <shada.kaalo@gmail.com>
2015-07-03 00:52:04 +0600

    ssss

--- include/Menu.h
+++ include/Menu.h
@@ -5,17 +5,24 @@ class Menu
 {
 protected:
 
+    Person* p;
 public:
 
-    void mainMenu()
+    void mainMenu(Person* p)
     {
+        this.p = p;
+
+        int ch;
         cout<<"1.Log in."<<endl;
         cout<<"2.Sign up."<<endl;
         cout<<"3.Forgot Password."<<endl;
+        cin>>ch;
+
+        if(ch==1)
     }
 
 private:
-    void display(Person* p)
+    void display()
     {
         if(p->getType() == "c") cMenu(p);
         if(p->getType() == "e") eMenu(p);
@@ -23,22 +30,38 @@ private:
         if(p->getType() == "p") pMenu(p);
     }
 
+<<<<<<< HEAD
     void cMenu(Person* p)
+=======
+    void customerMenu()
+>>>>>>> parent of a61c5c0... resolved
     {
 
     }
 
+<<<<<<< HEAD
     void eMenu(Person* p)
+=======
+    void employeeMenu()
+>>>>>>> parent of a61c5c0... resolved
     {
 
     }
 
+<<<<<<< HEAD
     void vMenu(Person* p)
+=======
+    void vendorMenu()
+>>>>>>> parent of a61c5c0... resolved
     {
 
     }
 
+<<<<<<< HEAD
     void pMenu(Person* p)
+=======
+    void personMenu()
+>>>>>>> parent of a61c5c0... resolved
     {
 
     }
